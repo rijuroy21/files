7e02c19f1bfbc6e29db7096db55326a3e1db493d
rijuroy21 <rijumonroy@gmail.com>
2024-11-14 11:25:21 +0530

    file added

---
+++ database/databse4.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+con=sqlite3.connect('/home/synnefo/Desktop/Riju/files/database/datbase4.db')
+
+try:
+    con.execute("create table std_details(admno int,name text,age int,email text)")
+except:
+    pass    
+
+# a=int(input("how many members do you want to add:"))
+# for i in range(a):
+#     admno=int(input("Enter the admno of the student:"))
+#     name=input("Enter name of the student:")
+#     age=int(input("Enter age of the student:"))
+#     email=input("Enter email of the student:")
+#     con.execute("insert into std_details values(?,?,?,?)",(admno,name,age,email))
+#     con.commit() 
+  
+# data=con.execute("select email,age from std_details where name='jithin'" )
+# for i in data:
+#     print(i)   
+
+# data=con.execute("select email,age from std_details where name like '%n'" )
+# for i in data:
+#     print(i)
+
+data=con.execute("select * from std_details" )
+print("{:<20}{:<20}{:<20}{:<20}".format("Admno","Name","Age","Email"))
+print("_"*90)
+for i in data:
+    print("{:<20}{:<20}{:<20}{:<20}".format(i[0],i[1],i[2],i[3]))
+    print("_"*90)
+c=con.execute('select max (age) from std_details')
+for i in c:
+    print(i)
--- database/omega.py
+++ database/omega.py
@@ -1,13 +1,11 @@
 import sqlite3
 
-con=sqlite3.connect("/home/synnefo/Desktop/riju/files/database/alpha.db")
+con=sqlite3.connect("/home/synnefo/Desktop/Riju/files/database/alpha.db")
 username=input("enter username:")
 password=input("enter password:")
-
-
 data=con.execute("select * from employee")
 for i in data:
     if i[5]==username and i[6]==password:
-        print("login successfull")
+        print("login successfully")
     # else:
     #     print("invalid username or password")
