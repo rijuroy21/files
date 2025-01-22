ba9ac234622b4b3fb42144186d87c59433335587
rijuroy21 <rijumonroy@gmail>
2025-01-22 11:28:55 +0530

    file added

---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+a= parseInt(prompt('Enter a number '));
+// b= parseInt(prompt('Enter a number '));
+// console.log(typeof (c));
+    (a%2==0) ? console.log(`${a} is even`) : console.log(`${a} is odd`)
